17b2ed61f401616f8f754d01d57d47a23c2f1bcd
Masab Haider <118460529+Masab2@users.noreply.github.com>
2024-04-15 09:42:37 -0800

    Changes by Masab

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,51 +4,61 @@ import 'package:diet_plan/models/recipe.dart';
 import 'package:diet_plan/recipeDetailPage.dart';
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 4, 34, 90),
-          title: const Text('Diet Planner'),
-          titleTextStyle: const TextStyle(
-          color: Colors.white ,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 4, 34, 90),
+        title: const Text('Diet Planner'),
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
           fontWeight: FontWeight.bold,
-          fontSize:23,
-          ),
+          fontSize: 23,
         ),
-        body: ListView.builder(
-          itemCount: Recipe.listofRecipes.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Column(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => 
-                    RecpieDetailPage(image: Recipe.listofRecipes[index].imageUrl, 
-                    title: Recipe.listofRecipes[index].recipename,),));
-
-                  }, 
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image(
-                        image: AssetImage(Recipe.listofRecipes[index].imageUrl),
+      ),
+      body: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        itemCount: Recipe.listofRecipes.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RecpieDetailPage(
+                        image: Recipe.listofRecipes[index].imageUrl,
+                        title: Recipe.listofRecipes[index].recipename,
+                        ingredients: Recipe.listofRecipes[index].ingredients,
                       ),
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image(
+                      image: AssetImage(Recipe.listofRecipes[index].imageUrl),
                     ),
                   ),
                 ),
-                Text(Recipe.listofRecipes[index].recipename,style: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ],
-            );
-          }
-        ),
-        );
+              ),
+              Text(
+                Recipe.listofRecipes[index].recipename,
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
--- lib/recipeDetailPage.dart
+++ lib/recipeDetailPage.dart
@@ -1,16 +1,54 @@
+import 'package:diet_plan/models/ingredients.dart';
 import 'package:flutter/material.dart';
 
 class RecpieDetailPage extends StatelessWidget {
-  final String  title, image;
-  const RecpieDetailPage({super.key, required this.image, required this.title});
+  final String title, image;
+  final List<Ingredients> ingredients;
+  const RecpieDetailPage(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.ingredients});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: const TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromARGB(255, 4, 34, 90),
+        centerTitle: true,
+      ),
       body: Column(
         children: [
-          const Text("Recipe Details"),
-          Text(title)
+          Image(
+            image: AssetImage(image),
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: ingredients.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Text(
+                    ingredients[index].quantity.toStringAsFixed(0),
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  title: Text(
+                    '${ingredients[index].name} / Measures : ${ingredients[index].meassure}',
+                  ),
+                );
+              },
+            ),
+          )
         ],
       ),
     );
